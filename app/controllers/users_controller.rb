35207a00449825f69676707bc387d5198abf90aa
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-11-01 10:55:46 -0400

    fill out get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
 
 	get '/login' do
-    erb :login
-  end
+     erb :login
+    end
+
+    post '/login' do 
+    	@user = User.find_by(email: params[:email]) #find user by params
+
+    	if @user.authenticate(params[:password]) #if valid then log in and create users session
+    		session[:user_id] = @user.id 
+    		redirect "users/#{@user.id}"
+    	else #redirect
+
+
+    	end
+    end
+
+    get 'users/id' do
+    	
+    end
+
 	
 
 end
