c297ee88ff3c9427478e1aeb26e0dd5d759f27db
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-11-10 17:15:20 -0500

    add get and post routes and entries

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,8 +1,20 @@
 class WorkoutsController < ApplicationController
 
 	get '/workouts/new' do 
-		erb :'/workouts/new'
-		
+		erb :'/workouts/new'		
 	end
+
+	post '/workouts' do
+		if !logged_in?
+			redirect '/'
+		if params[:content] != ""
+			user = User.find_by_id(session[:user_id])
+			@workout = Workout.create(params)
+			redirect "/workouts/#{@workout.id}"
+		else
+			redirect '/workouts/new'
+		 end
+	  end
+    end
 
 end
